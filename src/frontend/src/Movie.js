aef1ff30e0b1fcc1424e7228deb9e4bae5246903
MatAleksi <mattila.aleksi@hotmail.com>
2023-02-02 15:45:31 +0200

    Cookies work and movie page can be reached from url

--- src/frontend/src/Movie.js
+++ src/frontend/src/Movie.js
@@ -4,17 +4,21 @@ import ReactStars from "react-rating-stars-component"
 
 
 const GetMovieByID = (id) => {
+  
   const [movie, setMovie] = useState([])
-
+  
   useEffect(() => {   axios
     .get(`http://128.214.253.51:3000/dbgetgivenmoviedata?movieid=${id}`)
     .then(response => {
       setMovie(response.data)
     })
   }, []);
+  
   return (movie)
 }
 
+//Creates a new cookie with the movieid as the name of the cookie, 
+//rating as the value of the cookie. Exdays is the amount of days until the cookie expires
 function setCookie(movieid, rating, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -22,36 +26,51 @@ function setCookie(movieid, rating, exdays) {
   document.cookie = movieid + "=" + rating + ";" + expires + ";path=/";
 }
 
+//Searchers saved cookies for a cookie with the name movieid
+//Returns rating associated with that cookie or 0 if no cookie is found
 function getCookie(movieid) {
-  let name = movieid + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
+  let pairs = document.cookie.split(';');
+  for(let i = 0; i < pairs.length; i++) {
+    let pair = pairs[i].split('=');
+    if(pair[0].substring(0, 1) === ' '){
+      pair[0] = pair[0].substring(1)
+    }
+    if(pair[0] === movieid){
+      console.log(pair)
+      return pair[1];
+    }
   }
-  return "";
+  return 0;
 }
 
-let movId = 0
 
-const ratingStars = {
-  size: 40,
-  count: 5,
-  isHalf: false,
-  value: 0,
-  onChange: newValue => {
-    setCookie(movId, newValue, 5)
-    console.log(`Example 3: new value is ${newValue}`);
+const Movie = () => {
+  //Gets the movieid from the url
+  var urlString = window.location.href
+  var parseHelper = urlString.split('/movie/')   
+  var id = parseHelper[1]
+
+  const movie = GetMovieByID(id)
+  var movId = id
+  var stars = getCookie(movId)
+
+  const ratingStars = {
+    size: 40,
+    count: 5,
+    isHalf: false,
+    value: stars,
+    onChange: newValue => {
+      setCookie(movId, newValue, 5)
+    }
+  };
+  if(movie === []){
+    return(
+      <div class="page-container">
+        <h1>No movie found for movieID</h1>
+      </div>
+    )
   }
-};
 
-const Movie = ({ movie }) => {
-  movId = movie.movieid
   return (
     <div class="page-container">
       <h1>{movie.title}</h1>
