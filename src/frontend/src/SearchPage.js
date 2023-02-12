fad88f02e56c39457d83b97e611e22fbc6ed943f
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-02-12 23:06:53 +0200

    search page with sorting functions

--- src/frontend/src/SearchPage.js
+++ src/frontend/src/SearchPage.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
-import { useState, useEffect} from 'react'
-import DisplayMovie from './MovieCarusel'
+import { useState } from 'react'
+import { Link } from "react-router-dom"
+
+import image from './NoImage.jpg'
 
 const SearchPage = () => {
     const [searchResult, setSearchResult] = useState([])
@@ -21,22 +23,108 @@ const SearchPage = () => {
         })
     }
 
+    /*const handleSort = (event) => {
+        const types = {
+          releaseNew: handleSortByReleaseNew(),
+          releaseOld: handleSortByReleaseOld(),
+          titleAsc: handleSortByTitleAsc(),
+          titleDesc: handleSortByTitleAsc(),
+        }
+        const type = event.target.value
+        console.log('sort search', type)
+        if (type === 'releaseNew') {
+            types.releaseNew
+        } else if (type === 'releaseOld') {
+            types.releaseOld
+        } else if (type === 'titleAsc') {
+            types.titleAsc
+        } else {
+            types.titleDesc
+        }
+    }*/
+
+    const handleSortByReleaseOld = () => {
+        const sortedByReleaseNew = [...searchResult].sort(compareRelease)
+        setSearchResult(sortedByReleaseNew)
+        console.log(searchResult)
+    }
+
+    const handleSortByReleaseNew = () => {
+        const sortedByReleaseOld = [...searchResult].sort(compareRelease).reverse()
+        setSearchResult(sortedByReleaseOld)
+        console.log(searchResult)
+    }
+
+    const handleSortByTitleAsc = () => {
+        const sortedByTitleAsc = [...searchResult].sort((a, b) => a.title.localeCompare(b.title))
+        setSearchResult(sortedByTitleAsc)
+        console.log(searchResult)
+    }
+
+    const handleSortByTitleDesc = () => {
+        const sortedByTitleDesc = [...searchResult].sort((a, b) => a.title.localeCompare(b.title)).reverse()
+        setSearchResult(sortedByTitleDesc)
+        console.log(searchResult)
+    }
+
     return(
       <div class="page-container">
         <h2>Search movies</h2>
         <div>
             <form onSubmit={handleSearch}>
-                <label for="search">Search movies </label>
+                <label>Search movies </label>
                 <input value={newSearch} onChange={handleSearchChange} placeholder="Search movies"/>
             </form>
         </div>
         <div>
-          <ul>
-            {searchResult.map(movie => <li>{movie.title}</li>)}
-          </ul>
+            <p>
+                Sort by: 
+                <button onClick={handleSortByReleaseNew}>release newest first</button>
+                <button onClick={handleSortByReleaseOld}>release oldest first</button>
+                <button onClick={handleSortByTitleAsc}>title A-Z</button>
+                <button onClick={handleSortByTitleDesc}>title Z-A</button>
+                {/*<select onChange={handleSort}>
+                    <option value="releaseNew">Release newest first</option>
+                    <option value="releaseOld">Release oldest first</option>
+                    <option value="titleAsc">Title A-Z</option>
+                    <option value="titleDesc">Title Z-A</option>
+                </select>*/}
+            </p>
+        </div>
+        <div>
+            {searchResult.map(movie => <DisplayMovie key={movie.id} movie={movie}></DisplayMovie>)}
         </div>
       </div>
     )
 }
 
-  export default SearchPage
+export default SearchPage
+
+function compareRelease(a, b) {
+    if (Date.parse(a.releasedate) < Date.parse(b.releasedate)) {
+      return -1
+    } else if (Date.parse(a.releasedate) > Date.parse(b.releasedate)) {
+      return 1
+    }
+    return 0;
+  }
+
+const DisplayMovie = ({movie}) => {
+
+    var imageSource = `https://image.tmdb.org/t/p/original${movie.posterpath}`
+    if(movie.posterpath === null){
+        imageSource = image
+    }
+    return(
+        <div class="movie-slot">
+        <div  class='movie-pic'>
+            <Link to={`/movie/${movie.movieid}`}>
+                <img src={imageSource} />
+            </Link>
+        </div>
+        <div class="movie-info">
+            <Link to={`/movie/${movie.movieid}`}>{movie.title}</Link>
+        </div>
+        </div>
+    )
+}
