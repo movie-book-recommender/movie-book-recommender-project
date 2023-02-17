c997fd13e981e060faeed6e7ee39638dfb41c83c
Polina Nazarova <polina.w.nazarova@gmail.com>
2023-02-17 17:08:28 +0200

    tests for books

---
+++ src/frontend/cypress/e2e/books.cy.js
@@ -0,0 +1,50 @@
+describe("Itemlens ", function () {
+  beforeEach(function () {
+    cy.visit("http://localhost:3000");
+    cy.contains("You are on page movies switch to books").click();
+  });
+
+  // test if frontend can be opened
+  it("front page can be opened", function () {
+    cy.contains("Top 10 newest books");
+  });
+
+  // test if newest books are relevant
+  it("newest boooks are relevant", function () {
+    cy.contains("A Conjuring of Light (Shades of Magic, #3)");
+  });
+
+  // test if you can write in search field
+  it("can type in search and shows results", function () {
+    cy.get("input").type("rincewind");
+    cy.contains("Eric (Discworld, #9; Rincewind #4)");
+  });
+
+  // test if you can clear search field and previously searched movies are hiden
+  it("search input clears and hides previous search results", function () {
+    cy.get("input").type("rincewind");
+    cy.contains("Eric (Discworld, #9; Rincewind #4)");
+    cy.get("input").clear();
+    cy.contains("Eric (Discworld, #9; Rincewind #4)").should("not.exist");
+  });
+
+  it("search page opens", function () {
+    cy.contains("Search").click();
+    cy.contains("Search books");
+  });
+  it("search page opens and search for books", function () {
+    cy.contains("Search").click();
+    cy.contains("Search books");
+    cy.get("input").type("rincewind");
+    cy.contains("Eric (Discworld, #9; Rincewind #4)");
+  });
+
+  it("hides previous search", function () {
+    cy.contains("Search").click();
+    cy.contains("Search books");
+    cy.get("input").type("rincewind");
+    cy.contains("Eric (Discworld, #9; Rincewind #4)");
+    cy.get("input").clear();
+    cy.contains("Eric (Discworld, #9; Rincewind #4)").should("not.exist");
+  });
+});
--- src/frontend/cypress/e2e/searchPage.cy.js
+++ src/frontend/cypress/e2e/searchPage.cy.js
@@ -22,13 +22,4 @@ describe("Itemlens ", function () {
     cy.get("input").clear();
     cy.contains("Caesar Against the Pirates").should("not.exist");
   });
-  it("resease new sort works", function () {
-    cy.contains("Search").click();
-    cy.contains("Search movies");
-    cy.get("input").type("pirates");
-    cy.contains("Caesar Against the Pirates");
-    cy.contains("release newest first").click();
-    cy.contains("Caesar Against the Pirates").should("not.be.visible");
-    cy.contains("Pirates");
-  });
 });
