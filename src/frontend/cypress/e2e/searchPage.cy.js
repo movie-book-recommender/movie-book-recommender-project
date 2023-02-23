3f6552d0f2ec571859ab70140cd29881cb4d9081
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-02-23 13:49:32 +0200

    fix to searchpage tests

--- src/frontend/cypress/e2e/searchPage.cy.js
+++ src/frontend/cypress/e2e/searchPage.cy.js
@@ -28,22 +28,18 @@ describe("Search Page ", function () {
       cy.get("input").type("harry potter");
       cy.get("[data-testid='SearchIcon']").click()
     })
-    //These tests fail for some reason
-    //it("sorting by oldest first", function (){
-      //cy.contains("release oldest first").click()
-      //cy.contains("release oldest first").click()
-      //cy.get(".movie-slot").first().contains("Harry Potter and the Philosopher's Stone")
-    //})
 
     it("sorting by oldest first", {"retries": 2}, function (){
       cy.contains("release oldest first").click()
       cy.contains("release oldest first").click()
+      cy.reload()
       cy.get(".movie-slot").first().contains("Harry Potter and the Philosopher's Stone")
     })
 
     it("sorting by title descending",{"retries": 2}, function (){
       cy.contains("title Z-A").click()
       cy.contains("title Z-A").click()
+      cy.reload()
       cy.get(".movie-slot").first().contains("The Greater Good - Harry Potter Fan Film")
     })
   })
