39cb0ee0a216710a578014c11fec10dc491712e6
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-03-10 18:40:49 +0200

    tests for removing movie from wishlist

--- main/cypress/e2e/wishlist.cy.js
+++ main/cypress/e2e/wishlist.cy.js
@@ -12,4 +12,29 @@ describe("Wishlist ", function () {
         cy.get(".heart").should("have.length", 1)
     })
 
+    it("can remove movie from wishlist in Wishlist page", function (){
+      cy.wait(1000)
+      cy.get(".movie-pic").first().click()
+      cy.wait(1000)
+      cy.get(".heart").click()
+      cy.contains("Wishlist").click()
+      cy.get(".heart").should("have.length", 1)
+      cy.get(".heart").click()
+      cy.reload()
+      cy.get(".heart").should("have.length", 0)
+    })
+
+    it("can remove movie from wishlist in movie's page", function (){
+      cy.wait(1000)
+      cy.get(".movie-pic").first().click()
+      cy.wait(1000)
+      cy.get(".heart").click()
+      cy.contains("Wishlist").click()
+      cy.get(".heart").should("have.length", 1)
+      cy.get("img").first().click()
+      cy.get(".heart").click()
+      cy.contains("Wishlist").click()
+      cy.get(".heart").should("have.length", 0)
+    })
+
 })
