41216f9d15c2d36431226535ce2676c9a08dd668
MatAleksi <mattila.aleksi@hotmail.com>
2023-03-28 13:09:13 +0300

    ratigns page layout and cookie consent popup clean up

--- main/src/pages/Ratings.js
+++ main/src/pages/Ratings.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
 import { useState, useEffect } from "react";
-import { Modal, Box, Grid } from "@mui/material";
+import { Modal, Box, Button } from "@mui/material";
 
 import "../css/Ratings.css";
 import image from "../NoImage.jpg";
 import { getCookies, setCookie, removeAllRatings } from "../Cookies.js";
 import { GetMovieByID } from "../components/Movie";
 import { GetBookByID } from "../components/Book";
+import Table from "../Table"
 
 var cookiesB = getCookies("B");
 var cookiesM = getCookies("M");
@@ -38,31 +39,24 @@ const DisplayMovie = ({ id, rating }) => {
     updateCookies()
   }
   return (
-    <div>
-      <Box sx={{
-        bgcolor:'white',
-        height:400,
-        width: 250,
-        border:2,
-        borderColor: 'text.disabled',
-        mx:'auto',
-        textAlign:'Center'
-        
-      }}
-      >
+    <div class="table-item">
+      <div class="table-item-pic">
         <Link to={`/movie/${movie.movieid}`}>
           <img src={imageSource} width={150} height={"auto"} />
-        </Link>        
-        <Box sx={{
-          ml:4
-        }}>
+        </Link> 
+      </div>
+      <div class="table-item-info">
+        <div class="table-item-title">
+          <Link to={`/movie/${movie.movieid}`}>{movie.title}</Link>
+        </div>
+        <div class="table-item-rate">
           <ReactStars {...ratingStars} />
-        </Box>
+        </div>     
+          
         <Link onClick={() =>{removeRating("M", id)}}>
           <p>Remove rating</p>  
         </Link>
-        <h4>{movie.title}</h4>
-      </Box>
+      </div>
     </div>
   );
 };
@@ -90,35 +84,24 @@ const DisplayBook = ({ id, rating }) =>{
   } 
 
   return(
-    <div>
-      <Box sx={{
-        height:400,
-        width: 250,
-        border:2,
-        mx:'auto',
-        textAlign:'center',
-        borderColor: 'text.disabled',
-        textAlign:'Center',
-        bgcolor:'white',
-      }}
-      >
+    <div class="table-item">
+      <div class="table-item-pic">
         <Link to={`/book/${book.item_id}`}>
           <img src={imageSource} height={250} width={"auto"} />
-        </Link>        
-        <Box sx={{
-          ml: 4
-        }}>
+        </Link> 
+      </div>
+      <div class="table-item-info">
+        <div class="table-item-title">
+          <Link to={`/movie/${book.item_id}`}>{book.title}</Link>
+        </div>
+        <div class="table-item-rate">
           <ReactStars {...ratingStars} />
-        </Box>
-        <Box sx={{
-          my:'auto'
-        }}>
-          <Link onClick={() =>{removeRating("B", id)}}>
-            <p>Remove rating</p>
-          </Link>
-          <h4>{book.title}</h4>
-        </Box>
-      </Box>
+        </div>     
+          
+        <Link onClick={() =>{removeRating("B", id)}}>
+          <p>Remove rating</p>  
+        </Link>
+      </div>
     </div>
   )
 }
@@ -155,6 +138,20 @@ const Ratings = ({ page }) => {
     setOpen(false)
   }
 
+  const Render = () =>{
+    let books = cookiesB.map((cookie) => <DisplayBook id={cookie[0]} rating={cookie[1]} key={cookie[0]}/>)
+    let movies = cookiesM.map((cookie) => <DisplayMovie id={cookie[0]} rating={cookie[1]} key={cookie[0]}/>)
+    return (
+      <div>
+        <div class="rowC">
+          <RenderMovies/>
+          <RenderBooks/>
+        </div>
+        <Table movies={movies} books={books}/>
+      </div>
+    );
+  }
+  
   const RenderMovies = () =>{
     if (cookiesM.length === 0) {
       <Box sx={{textAlign:'center'}}>
@@ -184,24 +181,16 @@ const Ratings = ({ page }) => {
     }
     return (
       <div>
-        <Box sx={{textAlign:'center'}}>
-          <h2>My movie ratings</h2>
-          <h3>You have rated {cookiesM.length} movies.</h3>
-          <Link onClick={() =>{removeAll("M")}}>
+        <div class="rowC">
+          <h2>You have rated {cookiesM.length} movies</h2>
+          <Button onClick={() =>{removeAll("M")}}>
             <p>Remove all movie ratings</p>
-          </Link>
-        </Box>
-        <Grid container={true} direction={'row'} columns={3}>
-          {cookiesM.map((cookie) => (
-            <Grid xs={1} mx='auto' mt={1}>
-              <DisplayMovie id={cookie[0]} rating={cookie[1]} key={cookie[0]}/>
-            </Grid>
-          ))}
-        </Grid>
+          </Button>
+          </div>
       </div>
-    );
+    )
   }
-  
+
   const RenderBooks = () =>{
     if (cookiesB.length === 0) {
       <Box sx={{textAlign:'center'}}>
@@ -231,41 +220,20 @@ const Ratings = ({ page }) => {
     }
     return(
       <div>
-        <Box sx={{textAlign:'center'}}>
-          <h2>My book ratings</h2>
-          <h3>You have rated {cookiesB.length} books.</h3>
-          <Link onClick={() =>{removeAll("B")}}>
-            <p>Remove all book ratings</p>
-          </Link>
-        </Box>
-        <Grid container={true} direction={'row'} columns={3}>
-          {cookiesB.map((cookie) => (
-            <Grid xs={1} mx='auto' mt={1}>
-              <DisplayBook id={cookie[0]} rating={cookie[1]} key={cookie[0]}/>
-            </Grid>
-          ))}
-        </Grid>
+        <div class="rowC">
+          <h2>You have rated {cookiesB.length} books.</h2>
+          <Button onClick={() =>{removeAll("B")}}>
+            Remove all book ratings
+          </Button>
+        </div>
       </div>
       );
   }
 
   return (
-      <div className='rowC'>
-        <Box sx={{
-          mr:1,
-          bgcolor:'#8de6fc',
-        }}
-        >
-          <RenderMovies/>
-        </Box>
-        <Box sx={{
-          ml:1,
-          bgcolor:'#f8fac3'
-        }}
-        >
-          <RenderBooks/>
-        </Box>
-      </div>
+    <div>
+      <Render/> 
+    </div>
   )
 };
 
