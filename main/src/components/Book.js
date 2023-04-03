1e889cae63580d57d0fe7293127f19d4c30d0c54
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-04-03 11:53:51 +0300

    new design for book page

--- main/src/components/Book.js
+++ main/src/components/Book.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import { Card, CardContent, CardMedia } from "@mui/material"
 import ReactStars from "react-rating-stars-component";
 import Heart from "react-heart";
+
 import image from "../NoImage.jpg";
 import { getCookie, setCookie, onWishlist, addToWishlist } from "../Cookies.js";
 import { updateCookies } from "../pages/Ratings";
@@ -54,6 +55,20 @@ const GetMovieRecommendationsByID = (id) => {
   return movies;
 };
 
+const GetBooksByAuthor = (author) => {
+  const [books, setBooks] = useState([])
+  useEffect(() => {
+    axios
+      .get(
+        `http://128.214.253.51:3000//dbsearchbooksbyauthor?input=${author}`
+      )
+      .then((response) => {
+        setBooks(response.data)
+      })
+  }, [author])
+  return books
+}
+
 const Book = () => {
   var urlString = window.location.href;
   var parseHelper = urlString.split("/book/");
@@ -62,6 +77,7 @@ const Book = () => {
   const book = GetBookByID(id);
   const recommendationsBooks = GetBookRecommendationsByID(id);
   const recommendationsMovies = GetMovieRecommendationsByID(id);
+  const sameAuthor = GetBooksByAuthor(book.authors, id).filter(book => book.item_id !== id)
   var bookId = id;
   var stars = getCookie("B", bookId);
 
@@ -96,11 +112,7 @@ const Book = () => {
       </div>
     );
   }
-  var imageSource = book.img;
-  if (book.img === null) {
-    imageSource = image;
-  }
-
+  var imageSource = book.img ? book.img : image
   var description = book.description.replace(/\\n/g, ' ')
 
   const isRated = () =>{
@@ -118,42 +130,57 @@ const Book = () => {
   }
 
   return (
-    <div className="page-container">
+    <div className="book-page-wrapper">
       <h1>{book.title}</h1>
-      <div>
-        <img src={imageSource} height={250} width={"auto"} alt="book poster" />
+      <h5>Published in {book.year}</h5>
+      <Card class="book" sx={{ maxWidth: 200 }}>
+        <CardMedia>
+          <img class="large-item-pic" src={imageSource} alt="movie-poster" />
+          <div class="heart" style={{ width: "2rem", position: "relative", top: -300, left: 5 }}>
+            <Heart {...heartElement} style = {{fill: heart ? "red": "grey", stroke: "black"}} />
+          </div>
+        </CardMedia>
+        <CardContent>
+          <h3>Your rating:</h3>
+          <ReactStars {...ratingStars} />
+          <div>{isRated()}</div>
+          <a href={`${book.url}`} target="_blank" rel="noreferrer">
+            <p>Book</p>
+          </a>
+        </CardContent>
+      </Card>
+      <div class="box">
+        <h3>Summary of the plot:</h3>
+        <p>{description}</p>
+        <h3>Authors:</h3>
+        <p>
+          {book.authors}
+        </p>
       </div>
-      <h3>Your rating:</h3>
-      <div>{isRated()}</div>
-      <ReactStars {...ratingStars} />
-      <div class="heart" style={{ width: "2rem" }}>
-        <Heart {...heartElement} />
+      <div class="same-author">
+        <h3>Other books from author</h3>
+        {sameAuthor.length > 1 ? (
+          <Items items={sameAuthor} page={"books"} recommendation={false} />
+        ) : (
+          <p>no other books from author</p>
+        )}
       </div>
-      <h3>Authors:</h3>
-      <p>{book.authors}</p>
-      <h3>Year:</h3>
-      <p>{book.year}</p>
-
-      <h3>Description:</h3>
-      <p>{description}</p>
-
-      <a href={`${book.url}`} target="_blank" rel="noreferrer">
-        <p>Book</p>
-      </a>
-
-      <h3>Similar books</h3>
-
-      {recommendationsBooks.length > 0 ? (
-        <Items items={recommendationsBooks} page={"books"} recommendation={true} />
-      ) : (
-        <p>could not find similar books</p>
-      )}
-      <h3>Similar movies</h3>
-      {recommendationsMovies.length > 0 ? (
-        <Items items={recommendationsMovies} page={"movies"} recommendation={true}/>
-      ) : (
-        <p>could not find similiar movies</p>
-      )}
+      <div class="similar-books">
+        <h3>Similar books</h3>
+        {recommendationsBooks.length > 0 ? (
+          <Items items={recommendationsBooks} page={"books"} recommendation={true} />
+        ) : (
+          <p>could not find similar books</p>
+        )}
+      </div>
+      <div class="similar-movies">
+        <h3>Similar movies</h3>
+        {recommendationsMovies.length > 0 ? (
+          <Items items={recommendationsMovies} page={"movies"} recommendation={true}/>
+        ) : (
+          <p>could not find similiar movies</p>
+        )}
+      </div>
     </div>
   );
 };
