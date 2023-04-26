fc562e6de1e30907b56127dff73cdabe4f69ef86
sebastian-lehto <sebastian.lehto@gmail.com>
2023-04-26 18:37:59 +0300

    commenting front end code

--- main/src/pages/WishList.js
+++ main/src/pages/WishList.js
@@ -14,7 +14,7 @@ import { getMovieById } from "../components/Movie";
 import { getBookById } from "../components/Book";
 import Table from "../Table";
 
-//Fetch the wishlist from cookies and remove the last element
+//Fetch the wishlist from cookies and remove the last element.
 //The split function will leave an empty element as the last
 //member of the list, which is then popped.
 var cookies = getStringOfWishlist().split("&");
